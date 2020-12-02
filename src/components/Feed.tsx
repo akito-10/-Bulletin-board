f3b1135d56d7bc3cacb6ccd73355a9e871996fcf
github akito-10 <akt.fukuda@gmail.com>
2020-12-02 23:23:55 +0900

    投稿のデータをdbに保存

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import PostInput from "./PostInput";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { IconButton, Typography } from "@material-ui/core";
+import { auth } from "../firebase/firebase";
+import styles from "./Feed.module.css";
 
 const Feed: React.FC = () => {
   return (
     <div>
+      <IconButton
+        className={styles.feed_exitBtn}
+        onClick={async () => auth.signOut()}
+      >
+        <Typography component="div">Sign Out</Typography>
+        <ExitToAppIcon />
+      </IconButton>
       <PostInput />
     </div>
   );
--- src/components/PostInput.tsx
+++ src/components/PostInput.tsx
@@ -5,9 +5,14 @@ import {
   TextField,
   Theme,
   IconButton,
+  Avatar,
 } from "@material-ui/core";
 import styles from "./PostInput.module.css";
 import { AddAPhoto } from "@material-ui/icons";
+import { db, storage } from "../firebase/firebase";
+import firebase from "firebase/app";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -16,20 +21,77 @@ const useStyles = makeStyles((theme: Theme) =>
       margin: theme.spacing(3, 5),
     },
     input: {
-      marginBottom: theme.spacing(2),
-      marginRight: theme.spacing(3),
+      margin: theme.spacing(0, 3, 2, 3),
+      overflowWrap: "break-word",
     },
   })
 );
 
 const PostInput: React.FC = () => {
-  const [postText, setPostText] = useState("");
+  const user = useSelector(selectUser);
   const classes = useStyles();
+  const [postText, setPostText] = useState("");
+  const [postImage, setPostImage] = useState<File | null>(null);
+
+  const onChangeImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files![0]) {
+      setPostImage(e.target.files![0]);
+      e.target.value = "";
+    }
+  };
+
+  const sendPost = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (postImage) {
+      const S =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const N = 16;
+      const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join("");
+      const fileName = randomChar + "_" + postImage.name;
+
+      const uploadPostImg = storage.ref(`images/${fileName}`).put(postImage);
+      uploadPostImg.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        () => {},
+        (err) => {
+          alert(err.message);
+        },
+        async () => {
+          await storage
+            .ref("images")
+            .child(fileName)
+            .getDownloadURL()
+            .then(async (url) => {
+              await db.collection("posts").add({
+                avatar: user.photoUrl,
+                image: url,
+                text: postText,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                username: user.displayName,
+              });
+            });
+        }
+      );
+    } else {
+      db.collection("posts").add({
+        avatar: user.photoUrl,
+        image: "",
+        text: postText,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        username: user.displayName,
+      });
+    }
+    setPostImage(null);
+    setPostText("");
+  };
 
   return (
     <div>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendPost}>
         <div className={styles.postInput_form}>
+          <Avatar className={styles.postInput_avatar} src={user.photoUrl} />
           <TextField
             className={classes.input}
             label="共有したい考えを書いてみよう"
@@ -42,12 +104,24 @@ const PostInput: React.FC = () => {
           />
           <IconButton>
             <label>
-              <AddAPhoto />
-              <input className={styles.postInput_hiddenIcon} type="file" />
+              <AddAPhoto
+                className={
+                  postImage
+                    ? styles.postInput_addIconLoaded
+                    : styles.postInput_addIcon
+                }
+              />
+              <input
+                className={styles.postInput_hiddenIcon}
+                type="file"
+                onChange={onChangeImageHandler}
+              />
             </label>
           </IconButton>
         </div>
         <button
+          type="submit"
+          disabled={!postText}
           className={
             postText
               ? styles.postInput_sendBtn
