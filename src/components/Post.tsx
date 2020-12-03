28e04fdc88194eee9f9270ef4741fd9507fdc999
github akito-10 <akt.fukuda@gmail.com>
2020-12-03 17:27:08 +0900

    コメント機能の実装

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,26 @@
-import React from "react";
-import { Avatar } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
 import styles from "./Post.module.css";
+import { db } from "../firebase/firebase";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import firebase from "firebase/app";
+import { Avatar, IconButton, makeStyles } from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
+import SendIcon from "@material-ui/icons/Send";
 
 interface PROPS {
   postId: string;
   avatar: string;
   image: string;
+  text: string;
+  timestamp: any;
+  username: string;
+}
+
+interface COMMENT {
+  id: string;
+  avatar: string;
   text: string;
   timestamp: any;
   username: string;
@@ -22,10 +36,62 @@ const useStyles = makeStyles((theme) => ({
   topMargin: {
     marginTop: theme.spacing(3),
   },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    display: "inline-block",
+    marginRight: theme.spacing(1),
+  },
 }));
 
 const Post: React.FC<PROPS> = (props) => {
   const classes = useStyles();
+  const user = useSelector(selectUser);
+  const [comment, setComment] = useState("");
+  const [openComment, setOpenComment] = useState(false);
+  const [commentCount, setCommentCount] = useState<number>();
+  const [comments, setComments] = useState<COMMENT[]>([
+    {
+      id: "",
+      avatar: "",
+      text: "",
+      timestamp: null,
+      username: "",
+    },
+  ]);
+
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      .doc(props.postId)
+      .collection("comments")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setComments(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().username,
+          }))
+        );
+      });
+    return () => {
+      unSub();
+    };
+  }, [props.postId]);
+
+  const newComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    db.collection("posts").doc(props.postId).collection("comments").add({
+      avatar: user.photoUrl,
+      text: comment,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      username: user.displayName,
+    });
+    setComment("");
+  };
 
   return (
     <div className={classes.post}>
@@ -51,6 +117,48 @@ const Post: React.FC<PROPS> = (props) => {
                 <img src={props.image} alt="投稿画像" />
               </div>
             )}
+            <IconButton
+              className={styles.post_commentOpenIcons}
+              onClick={() => setOpenComment(!openComment)}
+            >
+              {openComment ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />}
+              <p>{`1件の返信`}</p>
+            </IconButton>
+            {openComment && (
+              <>
+                {comments.map((com) => (
+                  <div key={com.id} className={styles.post_comments}>
+                    <Avatar src={com.avatar} className={classes.small} />
+                    <span className={classes.rightMargin}>@{com.username}</span>
+                    <span>
+                      {new Date(com.timestamp?.toDate()).toLocaleString()}
+                    </span>
+                    <p>{com.text}</p>
+                  </div>
+                ))}
+              </>
+            )}
+            <form onSubmit={newComment}>
+              <input
+                className={styles.post_commentInput}
+                type="text"
+                placeholder="コメントしよう！"
+                value={comment}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setComment(e.target.value)
+                }
+              />
+              <IconButton type="submit">
+                <SendIcon
+                  fontSize="small"
+                  className={
+                    comment
+                      ? styles.post_submitIcon
+                      : styles.post_submitIconDisable
+                  }
+                />
+              </IconButton>
+            </form>
           </div>
         </div>
       </div>
