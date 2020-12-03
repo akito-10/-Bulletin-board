b156e376a9c4ef492ef944f7a80db70f24611803
github akito-10 <akt.fukuda@gmail.com>
2020-12-03 12:20:07 +0900

    投稿の表示、スタイルの作成

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PostInput from "./PostInput";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { IconButton, Typography } from "@material-ui/core";
-import { auth } from "../firebase/firebase";
+import { auth, db } from "../firebase/firebase";
 import styles from "./Feed.module.css";
+import Post from "./Post";
 
 const Feed: React.FC = () => {
+  const [posts, setPost] = useState([
+    {
+      id: "",
+      avatar: "",
+      image: "",
+      text: "",
+      timestamp: null,
+      username: "",
+    },
+  ]);
+
+  useEffect(() => {
+    const unSub = db
+      .collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPost(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            image: doc.data().image,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().username,
+          }))
+        );
+      });
+
+    return () => unSub();
+  }, []);
+
   return (
-    <div>
+    <div className={styles.feed_root}>
       <IconButton
         className={styles.feed_exitBtn}
         onClick={async () => auth.signOut()}
@@ -16,6 +48,21 @@ const Feed: React.FC = () => {
         <ExitToAppIcon />
       </IconButton>
       <PostInput />
+      {posts[0]?.id && (
+        <>
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              postId={post.id}
+              avatar={post.avatar}
+              image={post.image}
+              text={post.text}
+              timestamp={post.timestamp}
+              username={post.username}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,61 @@
 import React from "react";
+import { Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+import styles from "./Post.module.css";
 
-const Post: React.FC = () => {
-  return <div></div>;
+interface PROPS {
+  postId: string;
+  avatar: string;
+  image: string;
+  text: string;
+  timestamp: any;
+  username: string;
+}
+
+const useStyles = makeStyles((theme) => ({
+  post: {
+    margin: theme.spacing(3, 5),
+  },
+  smallRight: {
+    marginRight: theme.spacing(3),
+  },
+  smallTop: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const Post: React.FC<PROPS> = (props) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.post}>
+      <div className={styles.post_content}>
+        <div className={classes.smallTop}>
+          <Avatar src={props.avatar} className={classes.smallRight} />
+        </div>
+        <div>
+          <div className={styles.post_header}>
+            <h3>
+              <span className={classes.smallRight}>@{props.username}</span>
+              <span>
+                {new Date(props.timestamp?.toDate()).toLocaleString()}
+              </span>
+            </h3>
+          </div>
+          <div>
+            <p className={styles.post_text}>{props.text}</p>
+          </div>
+          <div>
+            {props.image && (
+              <div className={styles.post_image}>
+                <img src={props.image} alt="投稿画像" />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
